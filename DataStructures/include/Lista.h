95116b44babae21111b89e8a6ecab6031d3ddee9
Mirco Catalano <catalanomircosav@gmail.com>
2025-01-16 07:43:31 +0100

    Revert "Esercizi PDF 3.1"
    This reverts commit fa64e98268d4b9170485a5d7de658a8c5e87b194.

--- DataStructures/include/Lista.h
+++ DataStructures/include/Lista.h
@@ -30,19 +30,18 @@ namespace Lista {
 		pos primoLista() const { return 1; }
 		pos succLista(pos) const;
 		pos precLista(pos) const;
-		pos RicercaLista(T data);
-		// ? attributi pubblici
-		pos corrente;
 
 		void scriviLista(T, pos);
 		void insLista(T, pos);
 		void cancLista(pos);
 
-		void stampaLista();
-		void OrdinaLista();
+		void stampaLista(); // ! definita in Servizio.h
+
+		// ? attributi pubblici
+		pos corrente;
+		int size;
 
 	private:
-		int size;
 		Nodo<T>* head, * tail;
 	};
 }
@@ -192,74 +191,4 @@ void Lista::Lista<T>::cancLista(pos p) {
 
 	delete tmp;
 	size--;
-}
-
-
-// ! ESERCIZI
-template<typename T>
-typename Lista::Lista<T>::pos Lista::Lista<T>::RicercaLista(T data) {
-	// Ordina la lista
-	this->OrdinaLista();
-
-	// Posizione iniziale
-	corrente = this->primoLista();
-
-	Nodo<T>* tmp = head;
-
-	// Itera sulla lista per cercare il dato
-	while (corrente <= size && tmp != nullptr) {
-		if (tmp->leggiNodo() == data) {
-			return corrente; // Restituisci la posizione
-		}
-
-		corrente = this->succLista(corrente);
-		tmp = tmp->leggiNext();
-	}
-
-	// Se il dato non è trovato, restituisci -1
-	return -1;
-}
-
-
-template<typename T>
-void Lista::Lista<T>::OrdinaLista() {
-	assert(!listaVuota(), "Lista vuota.");
-	assert(size == 1, "Lista contiene un solo elemento.");
-	
-	bool scambiato;
-	do {
-		scambiato = false;
-		Nodo<T>* tmp = head;
-
-		// Itera sui nodi della lista per confrontare e scambiare valori
-		while (tmp->leggiNext() != nullptr) {
-			if (tmp->leggiNodo() > tmp->leggiNext()->leggiNodo()) {
-				// Scambia i valori tra tmp e il nodo successivo
-
-				T tempValue = tmp->leggiNodo();
-				tmp->scriviNodo(tmp->leggiNext()->leggiNodo());
-				tmp->leggiNext()->scriviNodo(tempValue);
-				scambiato = true;
-			}
-			tmp = tmp->leggiNext(); // Vai al prossimo nodo
-		}
-	} while (scambiato); // Continua finché ci sono stati scambi
-}
-
-template<typename T>
-void Lista::Lista<T>::stampaLista() {
-	assert((!listaVuota()), "Lista vuota.");
-
-	corrente = this->primoLista();
-	Nodo<T>* tmp = head;
-
-	// Itera finché tmp non è nullptr
-	while (corrente <= size && tmp != nullptr) {
-		std::cout << "[ " << tmp->leggiNodo() << " ]" << " -> ";
-		// Passa al nodo successivo
-		tmp = tmp->leggiNext();
-		corrente = this->succLista(corrente);
-	}
-
-	std::cout << "[ nullptr ] \n";
 }
